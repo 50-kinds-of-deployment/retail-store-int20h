99acea113545f5bed16cc9e50d3849548287298c
monukoni <kartoplya213@gmail.com>
2026-03-14 20:49:09 +0200

    adding argocd

---
+++ terraform/layers/app/main.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  create_namespace = true
+  namespace        = "argocd"
+
+  version = "9.3.7"
+
+  wait            = true
+  cleanup_on_fail = true
+}
