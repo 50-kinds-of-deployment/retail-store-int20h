cda21e3492dd4b1f20c9b0638937ffa58c40d3f1
monukoni <kartoplya213@gmail.com>
2026-03-15 00:39:48 +0200

    test

--- terraform/layers/bootstrap/main.tf
+++ terraform/layers/bootstrap/main.tf
@@ -21,6 +21,23 @@ resource "aws_s3_bucket" "reports_bucket" {
   }
 }
 
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.reports_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.reports_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "retail_store_bucket" {
   bucket = aws_s3_bucket.retail_store_bucket.id
 
